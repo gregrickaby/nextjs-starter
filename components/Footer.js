23b3d4d7b523fb03b7c53448092944ced565376c
Greg Rickaby <greg@gregrickaby.com>
2020-08-21 16:34:20 -0500

    add author

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,8 @@ import config from '@/lib/config'
 export default function Footer() {
   return (
     <footer className="text-center text-sm border-t-2 border-gray-400 pt-2">
-      &copy; {new Date().getFullYear()} {config.siteName} &middot;{' '}
+      &copy; {new Date().getFullYear()} {config.siteName} by {config.author}{' '}
+      &middot;{' '}
       <Link href={config.social.github.href}>
         <a>Github</a>
       </Link>{' '}
