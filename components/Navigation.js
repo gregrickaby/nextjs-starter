586f51fcb02b63e5969ca2a7edca18fc3dc67b9c
Greg Rickaby <greg@gregrickaby.com>
2020-08-23 17:21:12 -0500

    sticky + shrinking header

--- components/Navigation.js
+++ components/Navigation.js
@@ -3,10 +3,12 @@ import config from '@/lib/config'
 
 export default function Navigation() {
   return (
-    <nav className="flex items-center space-x-2">
+    <nav className="flex items-center">
       {config.navigation.map((item, index) => (
         <Link href={item.href} key={index}>
-          <a className="underline hover:no-underline">{item.label}</a>
+          <a className="ml-12 transition-colors duration-200 ease-in-out hover:text-gray-600">
+            {item.label}
+          </a>
         </Link>
       ))}
     </nav>
