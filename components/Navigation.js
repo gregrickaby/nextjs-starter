8b72e305cd32a5abe38d8f002ef7e0140722b8d4
Greg Rickaby <greg@gregrickaby.com>
2020-08-24 20:25:01 -0500

    add hamburger react

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,16 +1,24 @@
+import {useState} from 'react'
 import Link from 'next/link'
 import config from '@/lib/config'
+import Hamburger from 'hamburger-react'
 
 export default function Navigation() {
+  const [isOpen, setOpen] = useState(false)
   return (
     <nav className="flex items-center">
-      {config.navigation.map((item, index) => (
-        <Link href={item.href} key={index}>
-          <a className="ml-12 transition-colors duration-200 ease-in-out hover:text-gray-600">
-            {item.label}
-          </a>
-        </Link>
-      ))}
+      <div className="flex items-center justify-between">
+        {isOpen
+          ? config.navigation.map((item, index) => (
+              <Link href={item.href} key={index}>
+                <a className="ml-12 transition-colors duration-200 ease-in-out hover:text-gray-600">
+                  {item.label}
+                </a>
+              </Link>
+            ))
+          : null}
+        <Hamburger label="Show menu" toggled={isOpen} toggle={setOpen} />
+      </div>
     </nav>
   )
 }
