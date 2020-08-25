3b2181da2e1c68c714f58e468e62f86d59f4875d
Greg Rickaby <greg@gregrickaby.com>
2020-08-24 20:26:02 -0500

    remove extra div

--- components/Navigation.js
+++ components/Navigation.js
@@ -6,19 +6,17 @@ import Hamburger from 'hamburger-react'
 export default function Navigation() {
   const [isOpen, setOpen] = useState(false)
   return (
-    <nav className="flex items-center">
-      <div className="flex items-center justify-between">
-        {isOpen
-          ? config.navigation.map((item, index) => (
-              <Link href={item.href} key={index}>
-                <a className="ml-12 transition-colors duration-200 ease-in-out hover:text-gray-600">
-                  {item.label}
-                </a>
-              </Link>
-            ))
-          : null}
-        <Hamburger label="Show menu" toggled={isOpen} toggle={setOpen} />
-      </div>
+    <nav className="flex items-center justify-between">
+      {isOpen
+        ? config.navigation.map((item, index) => (
+            <Link href={item.href} key={index}>
+              <a className="ml-12 transition-colors duration-200 ease-in-out hover:text-gray-600">
+                {item.label}
+              </a>
+            </Link>
+          ))
+        : null}
+      <Hamburger label="Show menu" toggled={isOpen} toggle={setOpen} />
     </nav>
   )
 }
