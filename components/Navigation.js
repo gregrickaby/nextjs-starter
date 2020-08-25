3ac43102d6521f28730ddadf29d7d22642afde24
Greg Rickaby <greg@gregrickaby.com>
2020-08-24 21:21:27 -0500

    add navigation drawer

--- components/Navigation.js
+++ components/Navigation.js
@@ -3,20 +3,42 @@ import Link from 'next/link'
 import config from '@/lib/config'
 import Hamburger from 'hamburger-react'
 
+function Links() {
+  return (
+    <>
+      {config.navigation.map((item, index) => (
+        <Link href={item.href} key={index}>
+          <a className="ml-8 transition-colors duration-200 ease-in-out hover:text-gray-600">
+            {item.label}
+          </a>
+        </Link>
+      ))}
+    </>
+  )
+}
+
+function Drawer() {
+  return (
+    <div className="absolute w-3/5 h-screen py-12 px-4 top-0 right-0 flex flex-col bg-white shadow-md">
+      <Links />
+    </div>
+  )
+}
+// on mobile, display the burger, hide the links
+// on mobile, when the burger is clicked, display the drawer
+// on desktop, display the menu
+
 export default function Navigation() {
   const [isOpen, setOpen] = useState(false)
   return (
     <nav className="flex items-center justify-between">
-      {isOpen
-        ? config.navigation.map((item, index) => (
-            <Link href={item.href} key={index}>
-              <a className="ml-12 transition-colors duration-200 ease-in-out hover:text-gray-600">
-                {item.label}
-              </a>
-            </Link>
-          ))
-        : null}
-      <Hamburger label="Show menu" toggled={isOpen} toggle={setOpen} />
+      <div className="hidden md:block">
+        <Links />
+      </div>
+      {isOpen ? <Drawer /> : null}
+      <span className="md:hidden">
+        <Hamburger label="Show menu" toggled={isOpen} toggle={setOpen} />
+      </span>
     </nav>
   )
 }
