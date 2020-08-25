5e054eed35a5aff4bbfff3c38cb0f363148ec0ef
Greg Rickaby <greg@gregrickaby.com>
2020-08-24 20:25:09 -0500

    move logo to its own component

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,6 @@
 import {useEffect, useRef} from 'react'
-import config from '@/lib/config'
 import {shrinkHeader} from '@/lib/functions'
+import Logo from '@/components/logo'
 import Navigation from '@/components/Navigation'
 
 export default function Header() {
@@ -13,15 +13,9 @@ export default function Header() {
       ref={headerRef}
       className="sticky top-0 py-8 transition-all bg-white border-b border-gray-200"
     >
-      <div className="wrap">
-        <div className="flex items-center justify-between">
-          <div className="flex items-center space-x-4">
-            <img src="/logo.svg" alt="site logo" height="128" width="128" />
-            {/* <h1 className="hidden md:block font-bold">{config.siteName}</h1>
-          <p className="hidden md:block italic">{config.siteDescription}</p> */}
-          </div>
-          <Navigation />
-        </div>
+      <div className="wrap flex items-center justify-between">
+        <Logo />
+        <Navigation />
       </div>
     </header>
   )
