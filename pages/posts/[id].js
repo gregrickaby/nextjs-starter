a1b96509c1faa957aa3b34ec474ac4c19e601bda
Greg Rickaby <greg@gregrickaby.com>
2020-08-25 21:25:46 -0500

    add blog posts

---
+++ pages/posts/[id].js
@@ -0,0 +1,68 @@
+import PropTypes from 'prop-types'
+import Layout from '@/components/Layout'
+import {fetcher} from '@/lib/functions'
+
+export default function BlogPost({post}) {
+  return (
+    <Layout title={post.title.rendered} description={post.excerpt.rendered}>
+      <div
+        className="flex items-center bg-blue-500 text-white px-4 mb-8 rounded shadow"
+        role="alert"
+      >
+        <svg
+          className="fill-current w-4 h-4 mr-4"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 20 20"
+        >
+          <path d="M12.432 0c1.34 0 2.01.912 2.01 1.957 0 1.305-1.164 2.512-2.679 2.512-1.269 0-2.009-.75-1.974-1.99C9.789 1.436 10.67 0 12.432 0zM8.309 20c-1.058 0-1.833-.652-1.093-3.524l1.214-5.092c.211-.814.246-1.141 0-1.141-.317 0-1.689.562-2.502 1.117l-.528-.88c2.572-2.186 5.531-3.467 6.801-3.467 1.057 0 1.233 1.273.705 3.23l-1.391 5.352c-.246.945-.141 1.271.106 1.271.317 0 1.357-.392 2.379-1.207l.6.814C12.098 19.02 9.365 20 8.309 20z" />
+        </svg>
+        <p className="pt-4">
+          Static Generation (SSG) example via{' '}
+          <a href="https://nextjs.org/docs/basic-features/data-fetching#getstaticpaths-static-generation">
+            <strong>getStaticPaths()</strong>
+          </a>
+          . The data is sourced from the WordPress REST-API.
+        </p>
+      </div>
+
+      <section>
+        <article>
+          <h1>
+            <a
+              href={post.link}
+              dangerouslySetInnerHTML={{__html: post.title.rendered}}
+            />
+          </h1>
+          <div dangerouslySetInnerHTML={{__html: post.content.rendered}} />
+        </article>
+      </section>
+    </Layout>
+  )
+}
+
+export async function getStaticPaths() {
+  const posts = await fetcher('https://gregrickaby.com/wp-json/wp/v2/posts')
+
+  return {
+    paths: posts.map((post) => {
+      return {
+        params: {
+          id: `${post.id}`
+        }
+      }
+    }),
+    fallback: false
+  }
+}
+
+export async function getStaticProps({params}) {
+  const post = await fetcher(
+    `https://gregrickaby.com/wp-json/wp/v2/posts/${params.id}`
+  )
+
+  return {props: {post}}
+}
+
+BlogPost.propTypes = {
+  post: PropTypes.object
+}
