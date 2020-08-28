e9da7a6d08e3cd0725f9a056d47fc7cee2026cda
Greg Rickaby <greg@gregrickaby.com>
2020-08-28 07:50:57 -0500

    pass in bg string

--- pages/index.js
+++ pages/index.js
@@ -6,10 +6,7 @@ export default function HomePage() {
   return (
     <Layout title={config.siteTitle} description={config.siteDescription}>
       <Hero
-        background={{
-          backgroundImage:
-            'https://i.picsum.photos/id/1059/1280/720.jpg?hmac=3ajpkbtku_zopsq04qdo4zx56f7j3j_esi0aqhxgad8'
-        }}
+        background="https://images.unsplash.com/photo-1513106021000-168e5f56609d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1920&q=70"
         title="Next.js Starter"
         description="A slightly opinionated, yet still bare-bones Next.js starter."
       />
