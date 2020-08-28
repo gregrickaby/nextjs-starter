a82352678b597fa74aa957d2e701d120b0413acb
Greg Rickaby <greg@gregrickaby.com>
2020-08-27 20:07:14 -0500

    import hero

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,14 @@
 import config from '@/lib/config'
 import Layout from '@/components/global/Layout'
+import Hero from '@/components/shared/Hero'
 
 export default function HomePage() {
   return (
     <Layout title={config.siteTitle} description={config.siteDescription}>
-      <h1>Home</h1>
+      <Hero
+        title="Next.js Starter"
+        description="A slightly opinionated, yet still bare-bones Next.js starter."
+      />
     </Layout>
   )
 }
