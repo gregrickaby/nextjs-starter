a106b33f70037f0b85f3e2549daf036309ada79e
Greg Rickaby <greg@gregrickaby.com>
2020-08-28 08:07:32 -0500

    final touches on <Hero>

--- components/shared/Hero.js
+++ components/shared/Hero.js
@@ -3,17 +3,17 @@ import PropTypes from 'prop-types'
 
 export default function Hero(props) {
   return (
-    <section className="relative w-full">
-      <div className="relative z-10 flex flex-col text-center">
+    <section className="relative overflow-hidden">
+      <div className="container relative z-10 p-24 lg:p-64">
         <h2 className="text-4xl tracking-tight leading-10 font-extrabold text-gray-900 sm:text-5xl sm:leading-none md:text-6xl">
           {props.title}
         </h2>
-        <p className="text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
+        <p className="text-base text-gray-700 sm:text-lg md:text-xl">
           {props.description}
         </p>
       </div>
-      <div className="w-full h-full absolute top-0 z-0">
-        <img src={props.background} alt={props.title} loading="lazy" />
+      <div className="absolute top-0 z-0">
+        <img src={props.background} alt="" loading="lazy" />
       </div>
     </section>
   )
