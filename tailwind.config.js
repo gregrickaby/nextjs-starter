d96e7bc3728df4d99ec2041e9d65ab8203f976fe
Greg Rickaby <greg@gregrickaby.com>
2020-08-28 07:50:36 -0500

    configure container

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      container: {
+        center: true,
+        margin: 'auto'
+      }
+    }
   },
   variants: {},
   plugins: [],
