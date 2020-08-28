b199e2dda416199f30a77e46b1dc57c611a7a2ce
Greg Rickaby <greg@gregrickaby.com>
2020-08-28 07:50:43 -0500

    remove all styles

--- components/global/Layout.js
+++ components/global/Layout.js
@@ -6,24 +6,22 @@ import config from '@/lib/config'
 
 export default function Layout({children, ...props}) {
   return (
-    <div className="space-y-8">
+    <>
       <SiteHead title={props.title} description={props.description} />
       <Header />
-      <main className="site-content max-w-3xl mx-auto px-4 lg:px-0">
-        {children}
-      </main>
+      <main>{children}</main>
       <Footer />
-    </div>
+    </>
   )
 }
 
 Layout.defaultProps = {
-  title: config.siteName,
-  description: config.siteDescription
+  description: config.siteDescription,
+  title: config.siteName
 }
 
 Layout.propTypes = {
-  title: PropTypes.string,
+  children: PropTypes.array.isRequired,
   description: PropTypes.string,
-  children: PropTypes.array.isRequired
+  title: PropTypes.string
 }
