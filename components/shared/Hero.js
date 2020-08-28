48c5cfa24323578ab82b2b51c9aca35f08beb4d1
Greg Rickaby <greg@gregrickaby.com>
2020-08-27 20:07:06 -0500

    add hero component

---
+++ components/shared/Hero.js
@@ -0,0 +1,19 @@
+import PropTypes from 'prop-types'
+
+export default function Hero(props) {
+  return (
+    <section className="relative bg-white overflow-hidden">
+      <h2 className="text-4xl tracking-tight leading-10 font-extrabold text-gray-900 sm:text-5xl sm:leading-none md:text-6xl">
+        {props.title}
+      </h2>
+      <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
+        {props.description}
+      </p>
+    </section>
+  )
+}
+
+Hero.propTypes = {
+  title: PropTypes.string,
+  description: PropTypes.string
+}
