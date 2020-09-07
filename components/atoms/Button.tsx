19baba6281d0b23ffe2967281153b5b517d0984c
Greg Rickaby <greg@gregrickaby.com>
2020-09-07 07:48:56 -0500

    move Button to top

--- components/atoms/Button.tsx
+++ components/atoms/Button.tsx
@@ -2,36 +2,6 @@ import React from 'react'
 import Link from 'next/link'
 import cn from 'classnames'
 
-export interface LinkProps {
-  /**
-   * Required. The button text.
-   */
-  text: string
-  /**
-   * Required. The button link.
-   */
-  url: string
-  /**
-   * Optional. Additional CSS classes.
-   */
-  classes?: string
-}
-
-const defaultStyles =
-  'bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
-
-export const LinkButton: React.FC<LinkProps> = ({
-  text,
-  url,
-  classes
-}: LinkProps) => {
-  return (
-    <Link href={url}>
-      <a className={cn(defaultStyles, classes)}>{text}</a>
-    </Link>
-  )
-}
-
 export interface ButtonProps {
   /**
    * Required. Checks if the button should be disabled.
@@ -64,3 +34,33 @@ const Button: React.FC<ButtonProps> = ({
 }
 
 export default Button
+
+export interface LinkProps {
+  /**
+   * Required. The button text.
+   */
+  text: string
+  /**
+   * Required. The button link.
+   */
+  url: string
+  /**
+   * Optional. Additional CSS classes.
+   */
+  classes?: string
+}
+
+const defaultStyles =
+  'bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'
+
+export const LinkButton: React.FC<LinkProps> = ({
+  text,
+  url,
+  classes
+}: LinkProps) => {
+  return (
+    <Link href={url}>
+      <a className={cn(defaultStyles, classes)}>{text}</a>
+    </Link>
+  )
+}
