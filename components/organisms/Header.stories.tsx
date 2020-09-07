3ad1877f919f745b3a4f54334fa0ba93d998d747
Greg Rickaby <greg@gregrickaby.com>
2020-09-07 15:39:26 -0500

    add header story

---
+++ components/organisms/Header.stories.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import Header from './Header'
+
+/**
+ * The following is a story.
+ *
+ * It follows the "Component Story Format" (CSF), an open standard
+ * based on ES6 modules which is portable beyond Storybook. This is
+ * version 2 of that standard and uses args.
+ *
+ * @see https://storybook.js.org/docs/react/api/csf
+ * @see https://storybook.js.org/docs/react/writing-stories/introduction
+ * @see https://medium.com/storybookjs/introducing-storybook-args-2dadcdb777cc
+ */
+
+export default {
+  title: 'Design System/Organisms/Header',
+  component: Header,
+  parameters: {
+    docs: {
+      description: {
+        component: 'The site header.'
+      }
+    }
+  }
+}
+
+// @ts-ignore
+const Template = () => <Header />
+
+export const Default = Template.bind({})
