a68c5d8fb8abc1e3bf1f0227b4f1de6d0c2f48dd
Greg Rickaby <greg@gregrickaby.com>
2020-09-08 13:24:44 -0500

    refactor/simplify

--- hooks/useScrollPosition.ts
+++ hooks/useScrollPosition.ts
@@ -2,33 +2,23 @@ import {useEffect, useState} from 'react'
 
 /**
  * Determine and return scroll position.
+ *
+ * @return {number} The current scroll position in pixels.
  */
-export default function useScrollPosition() {
+export default function useScrollPosition(): number {
   const [scrollPosition, setScrollPosition] = useState(0)
-  const [ticking, setTicking] = useState(false)
 
   useEffect(() => {
-    let last_known_scroll_position = 0
-
-    const handleScroll = (element: Event) => {
-      last_known_scroll_position = window.scrollY
-
-      if (ticking === false) {
-        window.requestAnimationFrame(() => {
-          setScrollPosition(last_known_scroll_position)
-          setTicking(false)
-        })
-
-        setTicking(true)
-      }
+    const handleScroll = () => {
+      setScrollPosition(window.pageYOffset)
     }
 
-    window.addEventListener('scroll', handleScroll)
+    window.addEventListener('scroll', handleScroll, {passive: true})
 
     return () => {
       window.removeEventListener('scroll', handleScroll)
     }
-  })
+  }, [])
 
   return scrollPosition
 }
