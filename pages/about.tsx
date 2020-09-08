32fcc62e6174b29b19323d5673f7825196d80571
Greg Rickaby <greg@gregrickaby.com>
2020-09-08 14:43:04 -0500

    coffee ipsum

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,42 +1,43 @@
 import React from 'react'
 import Layout from '@/components/common/Layout'
-import {Info} from '@/components/molecules/Alert'
 
 const About: React.FC = () => {
   return (
     <Layout title="About" description="Learn more about this website">
       <div className="container">
         <h1>About</h1>
-
-        <Info>
-          This is a basic page.{' '}
-          <a href="https://nextjs.org/docs/basic-features/pages">
-            Learn more about pages.
-          </a>
-        </Info>
-
-        <p>I love standards... but hate repeating myself.</p>
         <p>
-          For every new Next.js project I would literally copy & paste the same
-          configurations, pages, and components from previous projects into a
-          new one. After some research, I discovered that{' '}
-          <code>create-next-app</code>{' '}
-          <a href="https://www.npmjs.com/package/create-next-app#options">
-            works with git repositories!
-          </a>{' '}
+          Qui affogato arabica as caramelization lungo saucer so mug bar milk
+          kopi. Sugar single cream bar at mocha turkish lait. Brewed turkish
+          grounds et ut origin cortado strong kopi froth origin. Spoon a half
+          rich robust robusta affogato pot beans percolator sweet trifecta.
+          French americano beans acerbic siphon skinny filter trifecta sit half
+          luwak filter grounds that.
         </p>
         <p>
-          <strong>
-            I decided to create a slightly opinionated, yet still bare-bones
-            Next.js starter that I could use when starting new projects.
-          </strong>
+          Black to cup cortado rich percolator saucer irish trade. Sugar pumpkin
+          java medium shot est brewed cultivar robust mocha brewed panna spoon.
+          Percolator mocha viennese foam eu latte press beans espresso crema
+          single plunger variety. That aftertaste cinnamon bar trifecta rich
+          grounds seasonal cream irish froth foam spice skinny. Steamed saucer
+          so flavour plunger dark spoon spice wings qui redeye seasonal est.
         </p>
         <p>
-          Click the menu above to see the demo pages and please consider giving
-          the Github repo a star:{' '}
-          <a href="https://github.com/gregrickaby/nextjs-starter">
-            https://github.com/gregrickaby/nextjs-starter
-          </a>
+          Eu medium sugar foam medium half id americano coffee turkish. Variety
+          shot filter shot cortado americano in strong kopi shot. In plunger
+          cinnamon grinder doppio decaffeinated whipped viennese foam and dark.
+          Crema aged redeye robust instant cortado whipped shot brewed lungo
+          spoon americano aromatic. Aroma redeye that turkish latte variety
+          french cortado et au caramelization barista viennese.
+        </p>
+        <p>
+          Café as mountain galão at siphon in americano wings est cultivar spice
+          affogato. Trade kopi press arabica luwak wings single froth
+          frappuccino a grinder panna roast. Lungo filter iced a chicory et
+          aftertaste viennese percolator viennese froth ut saucer cultivar. Kopi
+          frappuccino redeye spice cultivar iced mazagran bar flavour luwak.
+          Cultivar au cream aromatic skinny at au et irish in black filter spice
+          au cream.
         </p>
       </div>
     </Layout>
