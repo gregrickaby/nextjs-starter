f1e311fc6ba4e2f883e1cf4aecd7ab555ea5c9ec
Greg Rickaby <greg@gregrickaby.com>
2020-09-08 12:36:56 -0500

    remove shrink header function

--- lib/functions.ts
+++ lib/functions.ts
@@ -1,20 +1,3 @@
 // Generic data fetcher.
 // @ts-ignore: (...args) input not provided
 export const fetcher = (...args) => fetch(...args).then((res) => res.json())
-
-/**
- * On scroll, add or remove a "shrink" class.
- *
- * @param {object} el The header element.
- */
-export function shrinkHeader(el) {
-  /* eslint-disable */
-  window.addEventListener('scroll', () => {
-    if (window.scrollY > 30) {
-      el.current.classList.add('shrink')
-    } else {
-      el.current.classList.remove('shrink')
-    }
-  })
-  /* eslint-enable */
-}
