49332a9e0455994d4bad3fa666ca8e364b1f2a19
Greg Rickaby <gregrickaby@gmail.com>
2020-12-21 08:23:59 -0600

    remove core web vitals

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,6 @@
 import PropTypes from 'prop-types'
 import '@/styles/index.css'
 
-/**
- * Support for Core Web Vitals.
- *
- * Note: Delete this if you're not interested in using.
- *
- * @see https://nextjs.org/blog/next-9-4#integrated-web-vitals-reporting
- */
-export function reportWebVitals(metric) {
-  console.log(metric)
-}
-
 const App = ({Component, pageProps}) => <Component {...pageProps} />
 
 App.propTypes = {
