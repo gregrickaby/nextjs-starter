1f90917333ddf3c7ced25d3ce0dbec9b4488a2e7
Greg Rickaby <gregrickaby@gmail.com>
2020-12-21 08:22:14 -0600

    ignore layer

--- stylelint.config.js
+++ stylelint.config.js
@@ -4,7 +4,14 @@ module.exports = {
     'at-rule-no-unknown': [
       true,
       {
-        ignoreAtRules: ['tailwind', 'apply', 'variants', 'responsive', 'screen']
+        ignoreAtRules: [
+          'tailwind',
+          'layer',
+          'apply',
+          'variants',
+          'responsive',
+          'screen'
+        ]
       }
     ],
     'declaration-block-trailing-semicolon': null,
