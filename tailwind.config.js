c642a32cad825950e363f3e8f61a33f1ca8e5aa2
Greg Rickaby <gregrickaby@gmail.com>
2020-12-21 08:22:26 -0600

    add safelist and font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,23 @@
 module.exports = {
-  purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
+  purge: {
+    content: [
+      './components/**/*.{js,ts,jsx,tsx,mdx}',
+      './pages/**/*.{js,ts,jsx,tsx}'
+    ],
+    options: {
+      safelistPatterns: [/^(bg-)/, /^(text-)/, /^(w-)/, /^(noUi-)/]
+    }
+  },
   darkMode: 'media',
   theme: {
     extend: {
       container: {
         center: true,
         margin: 'auto'
+      },
+      fontFamily: {
+        sans: ['Libre Franklin', 'sans-serif'],
+        serif: ['Libre Baskerville', 'serif']
       }
     }
   },
